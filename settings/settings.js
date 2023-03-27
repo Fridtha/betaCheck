2f709e9987e3ce6a5cf56d38b8082210f162cc9a
Fridtha <124099203+Fridtha@users.noreply.github.com>
2023-03-27 09:00:33 -0500

    Domain Handler in progress

--- settings/settings.js
+++ settings/settings.js
@@ -3,22 +3,18 @@
 //=============================================================================
 /*
  * Handles the settings for BetaCheck
+ * console.log();
 */
 //=============================================================================
 // Initialize the domain list arrays
-let allowedDomains = [];
-let forcedDomains = [];
+let allowedDomainsArray = [];
+let forcedDomainsArray = [];
+
 // let allowedDomains = ["pennstate", "learnpracticeplay", "overapi", "testallow"];
 // let forcedDomains = ["grydnet", "unsplash", "pinterest", "testforced"];
-// console.log(`Orig Array Allowed: ${allowedDomains}`);
-// console.log(`Orig Array Forced: ${forcedDomains}`);
-// saveDomains();
-// var allowedDString;
-
-
 
 // When the data is gotten
-function onGot(item) {
+function onGotLocalStorage(item) {
     // console.log(item);
 
     // DOMAIN LISTS
@@ -30,9 +26,94 @@ function onGot(item) {
 
 }
 
-// Promisory error
-function onError(error) {
-    console.log(`Error: ${error}`);
+function handleDomainLists(allowedDomainData, forcedDomainData) {
+    console.log(`allowedList String: ${allowedDomainData}`);
+    console.log(`forcedList String: ${forcedDomainData}`);
+
+    // Create the arrays and populate the lists on the page.
+    let parsedAllow = JSON.parse(allowedDomainData);
+    for (let i = 0; i < parsedAllow.length; i++) {
+        const domain = parsedAllow[i];
+        // console.log(`Allowed: ${parsedAllow[i]}`);
+        allowedDomainsArray.push(domain);
+        addListItemDomain("allowedlist", domain);
+    }
+    let parsedForced = JSON.parse(forcedDomainData);
+    for (let i = 0; i < parsedForced.length; i++) {
+        const domain = parsedForced[i];
+        // console.log(`Forced: ${domain}`);
+        forcedDomainsArray.push(domain);
+        addListItemDomain("forcedlist", domain);
+    }
+
+    console.log(`allowedDomainsArray: ${allowedDomainsArray}`);
+    console.log(`forcedDomainsArray: ${forcedDomainsArray}`);
+
+    document.addEventListener("click", (e) => {
+        let targetID = e.target.id;
+        let targetClass = e.target.className;
+        // console.log(`ID: ${targetID} || Class: ${targetClass}`);
+        // If clicked on...
+        // console.log();
+        // console.log(`Click! ${target}`);
+
+        // Reload button
+        if (e.target.id === `btn_reload`) {
+            window.location.reload();
+        }
+
+        // Test button
+        else if (e.target.id === `btn_test`) {
+            console.log();
+        }
+
+        // Add Allowed button
+        else if (e.target.id === `btn_addallowed`) {
+            // console.log(targetID);
+            // Get allowed input field value
+            let inputValue = document.getElementById(`input_addallow`).value;
+
+            // Check to see if the field is empty
+            const checkedValue = isEmptyOrSpaces(inputValue);
+            if (checkedValue) {
+                // If it's empty only show the alert.
+                showElement(`allowedAlert`);
+            } else {
+                // If it's not empty add it to the list and add the LI element
+                hideElement(`allowedAlert`);
+                addListItemDomain("allowedlist", inputValue);
+            }
+        }
+
+        // Add Forced button
+        else if (e.target.id === `btn_addforced`) {
+            // console.log(targetID);
+            // Get allowed input field value
+            let inputValue = document.getElementById(`input_addforced`).value;
+
+            // Check to see if the field is empty
+            const checkedValue = isEmptyOrSpaces(inputValue);
+            if (checkedValue) {
+                // If it's empty only show the alert.
+                showElement(`forcedAlert`);
+            } else {
+                // If it's not empty add it to the list and add the LI element
+                hideElement(`forcedAlert`);
+                addListItemDomain("forcedlist", inputValue);
+            }
+        }
+
+        // Remove button
+        else if (targetClass === `btn_remove`) {
+            const listItemDomain = e.target.previousElementSibling.innerText;
+            // console.log(`LiDomain: ${listItemDomain}`);
+            const lielement = e.target.parentElement;
+
+            removeListItemDomain(listItemDomain);
+            removeLiElement(lielement);
+
+        }
+    });
 }
 
 function saveDomains() {
@@ -46,36 +127,60 @@ function saveDomains() {
     }).then(r => console.log(`Forced domains saved`));
 }
 
-function handleDomainLists(allowedList, forcedList) {
-
-    let parsedAllow = JSON.parse(allowedList);
-    for (let i = 0; i < parsedAllow.length; i++) {
-        const domain = parsedAllow[i];
-        // console.log(`Allowed: ${parsedAllow[i]}`);
-        allowedDomains.push(domain);
-        addListItem("allowedlist", domain);
-    }
-
-    let parsedForced = JSON.parse(forcedList);
-    for (let i = 0; i < parsedForced.length; i++) {
-        const domain = parsedForced[i];
-        console.log(`Forced: ${domain}`);
-        forcedDomains.push(domain);
-        addListItem("forcedlist", domain);
-    }
-
-    // console.log(`Appended Array Allowed: ${allowedDomains}`);
-    // console.log(`Appended Array Forced: ${forcedDomains}`);
-}
-
-function addListItem(listId, item) {
-    //
+function addListItemDomain(listId, item) {
+    const licontent = `<div class=\"domainname\">${item}</div>\n<button class=\"btn_remove\">Remove</button>`;
     const ul = document.getElementById(listId);
     const li = document.createElement("li");
-    li.appendChild(document.createTextNode(item));
+    li.innerHTML = licontent;
     ul.appendChild(li);
+
+    // TODO Add item to array
+}
+
+function removeListItemDomain(listItemDomain) {
+    console.log(`Removing: ${listItemDomain}`);
+
+    // TODO Remove item from ARRAY
+}
+
+function removeLiElement(lielement) {
+    lielement.remove();
+    // fulfill(result); //if the action succeeded
+    // reject(error); //if the action did not succeed
+}
+
+function isEmptyOrSpaces(str){
+    // return str === null || str.match(/^ *$/) !== null;
+    // return str === null || str.match(/^\s*$/) !== null;
+    return str === null || (/^\s*$/).test(str);
+}
+
+function showStuff(id, text, btn) {
+    document.getElementById(id).style.display = `block`;
+    // hide the lorem ipsum text
+    document.getElementById(text).style.display = `none`;
+    // hide the link
+    btn.style.display = 'none';
+}
+
+function showElement(id) {
+    console.log(`Show Alert ${id}`);
+    let element = document.getElementById(id);
+    element.style.display = `block`;
+}
+function hideElement(id) {
+    console.log(`Hide Alert ${id}`);
+    let element = document.getElementById(id);
+    element.style.display = `none`;
+}
+
+// Promisory error
+function onError(error) {
+    console.log(`Error: ${error}`);
 }
 
 // Get the local storage data
 let gettingStoredData = browser.storage.local.get();
-gettingStoredData.then(onGot, onError);
+gettingStoredData.then(onGotLocalStorage, onError);
+
+
