8f92b134027498516d18a90ccde3bf346a44c480
Fridtha <124099203+Fridtha@users.noreply.github.com>
2023-03-28 20:42:06 -0500

    got domain check complete on each page

--- background.js
+++ background.js
@@ -1,20 +1,31 @@
-// TODO affect the chosen sites based on the domain lists in local storage.
+//=============================================================================
+// background.js v0.01
+//=============================================================================
+/*
+ * Runs in the backgroud of the Extension itself.
+*/
+//=============================================================================
+// console.log();
 
-// function onGotLocalStorageBack(tabs) {
-//     let tab_num, tab_url, tab_tit;
-//
-//     for (let tab of tabs) {
-//         tab_num = tab.index;
-//         tab_url = tab.url;
-//         tab_tit = tab.title;
-//         console.log(`Tab #: ${tab_num + 1} || Title: ${tab_tit} || URL: ${tab_url}`);
-//     }
-//     console.log(tabs);
-// }
-//
-// function onError(error) {
-//     console.log(`Error: ${error}`);
-// }
-//
-// let gettingCurrent = browser.tabs.query({ currentWindow: true });
-// gettingCurrent.then(onGotLocalStorageBack, onError);
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM Loaded in background.js");
+});
+
+function onGotLocalStorageBack(tabs) {
+    let tab_num, tab_url, tab_tit;
+
+    for (let tab of tabs) {
+        tab_num = tab.index;
+        tab_url = tab.url;
+        tab_tit = tab.title;
+        console.log(`Tab #: ${tab_num + 1} || Title: ${tab_tit} || URL: ${tab_url}`);
+    }
+    console.log(tabs);
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
+let gettingCurrent = browser.tabs.query({ currentWindow: true });
+gettingCurrent.then(onGotLocalStorageBack, onError);
--- betacheck.js
+++ betacheck.js
@@ -2,52 +2,98 @@
 // betacheck.js v0.01
 //=============================================================================
 /*
- * Reads the border color from storage.
+ * Affects the content of a page.
 */
 //=============================================================================
 // console.log();
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("START");
+const curURL = document.location.href;
 
 
-
-});
-
-function onGotTabs(tabs) {
-    let tab_num, tab_url, tab_tit;
-
-    for (let tab of tabs) {
-        tab_id = tab.id;
-        tab_num = tab.index;
-        tab_url = tab.url;
-        tab_tit = tab.title;
-        // console.log(`Tab #: ${tab_num + 1} || Title: ${tab_tit} || URL: ${tab_url}`);
-        // console.log(``);
-    }
-    console.log(tabs);
-
-
+function extractStorageData() {
+    const gettingLocalData = browser.storage.local.get();  // Gets the local storage data
+    gettingLocalData.then(onGotData, onError);  // Once it has the data, it runs onGotData
 }
 
-function onGotColor(item) {
-    let color = "blue";
-    if (item.color) {
-        color = item.color;
+// Once the local storage has been grabbed...
+function onGotData(item) {
+    let allowedList = [];
+    let forcedList = [];
+    let color = "darkslateblue"; // Sets darkslateblue as the default
+    if (item.color) { // however, if color exists in the local storage data, then...
+        color = item.color; // set color to what's in the data.
+    }
+    // console.log(`Current URL: ${curURL}`);
+
+    // Create the arrays and populate the lists on the page.
+    let parsedAllow = JSON.parse(item.allowedList);
+    for (let i = 0; i < parsedAllow.length; i++) {
+        const domain = parsedAllow[i];
+        allowedList.push(domain);
+    }
+    let parsedForced = JSON.parse(item.forcedList);
+    for (let i = 0; i < parsedForced.length; i++) {
+        const domain = parsedForced[i];
+        forcedList.push(domain);
     }
 
-    console.log(`Got color as ${color}`);
-    document.body.style.border = `10px solid ${color}`;
+    // const substrings = ["one", "two", "three"];
+    // let str;
+
+    // ------------------------
+    // Setup
+    // console.log(`Substrings: ${allowedList}`);
+
+    // Try it where we expect a match
+    // str = "this has one";
+    // console.log(curURL);
+    console.log(allowedList);
+    if (allowedList.some(v => curURL.includes(v))) {
+        console.log(`Allowed Match using "${curURL}"`);
+        // if url string includes allowed elements, do this
+        color = "mediumseagreen";
+    } else {
+        console.log(`No Allowed match using "${curURL}"`);
+    }
+
+    // Try it where we DON'T expect a match
+    // str = "this doesn't have any";
+    // console.log(curURL);
+    console.log(forcedList);
+    if (forcedList.some(v => curURL.includes(v))) {
+        console.log(`Forced Match using "${curURL}"`);
+        // else if domains include forced elements, do this
+        color = "crimson";
+    } else {
+        console.log(`No Forced match using "${curURL}"`);
+    }
+    // ------------------------
+
+
+
+
+
+    defineBorderColor(color);
+}
+function replace_in_element(element, word_obj) {
+    for (key in word_obj) {
+        console.log(key);
+        let rgx = new RegExp(key, "ig");
+        console.log(rgx);
+        element.nodeValue = element.nodeValue.replace(rgx, word_obj[key]);
+        // console.log("end");
+    }
+}
+
+function defineBorderColor(color) {
+    document.body.style.border = `10px solid ${color}`;  // Create the border
+}
+
+function myInitCode(message) {
+    console.log(message);
 }
 
 function onError(error) {
     console.log(`Error: ${error}`);
 }
 
-// Saves to the local comuter instance
-const getting = browser.storage.local.get("color");
-getting.then(onGotColor, onError);
-
-let gettingTabs = browser.tabs.query({ currentWindow: true });
-gettingTabs.then(onGotTabs, onError);
-
-
+extractStorageData();
